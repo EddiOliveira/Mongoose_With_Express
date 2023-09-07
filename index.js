9c0bd6e828790f55292c3589a235c18e22b2959e
Eddi Oliveira <oliveira.eduardo@estudante.ufjf.br>
2023-09-07 16:32:07 -0300

    Criando arquivo index.ejs que recebe todos os dados da collection products e renderiza. No arquivo index.js todos os dados da collection products são enviados para o arquivo index.ejs e solicita a renderização do mesmo

--- index.js
+++ index.js
@@ -19,8 +19,7 @@ app.set('view engine', 'ejs');
 
 app.get('/products', async (req, res) => {
     const products = await Product.find({});
-    console.log(products);
-    res.send('ALL PRODUCTS WILL BE HERE');
+    res.render('products/index', {products});
 })
 
 app.listen(3000, () => {
